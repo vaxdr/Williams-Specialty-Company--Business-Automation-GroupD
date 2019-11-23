c03626773db78b333367f31cab816081252bd07c
Joeyrmuzzo <57329326+Joeyrmuzzo@users.noreply.github.com>
2019-11-23 16:11:36 -0500

    Add files via upload

---
+++ week 4 login + MyAccount Updated/Williams Specialty Company/App_Code/clsDataLayer.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.OleDb;
+using System.Net;
+using System.Data;
+using System.Configuration;
+
+/// <summary>
+/// Summary description for clsDataLayer
+/// </summary>
+public class clsDataLayer
+{
+    public clsDataLayer()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    //Added 11/19/19
+    public static dsCustomers GetAddressInfo(string Database, string UserLogon)
+    {
+        dsCustomers DS;
+        OleDbConnection sqlConn;
+        OleDbDataAdapter sqlDA;
+
+
+        sqlConn = new OleDbConnection("PROVIDER=Microsoft.ACE.OLEDB.12.0;" +
+        "Data Source=" + Database); 
+
+        sqlDA = new OleDbDataAdapter("SELECT Customer.* FROM Users INNER JOIN Customer ON Users.UserID = Customer.UserID WHERE (((Users.UserLogon)= '" + UserLogon + "'))", sqlConn);
+
+        DS = new dsCustomers();
+        sqlDA.Fill(DS.Customer);
+        return DS;
+    }
+
+    //Added 11/18/19
+    public static dsUser VerifyUser(string Database, string UserName, string UserPassword)
+    {
+
+        dsUser DS;
+        OleDbConnection sqlConn;
+        OleDbDataAdapter sqlDA;
+
+
+        sqlConn = new OleDbConnection("PROVIDER=Microsoft.ACE.OLEDB.12.0;" +
+        "Data Source=" + Database);
+
+
+        sqlDA = new OleDbDataAdapter("Select UserSecLevel from Users " +
+                                      "where UserLogon like '" + UserName + "' " +
+                                      "and UserPassword like '" + UserPassword + "'", sqlConn);
+
+
+        DS = new dsUser();
+        sqlDA.Fill(DS.Users);
+        return DS;
+
+    }
+ //end 11/18/19 addition
+
+    public static bool SaveUser(string Database, string UserLogon, string UserPassword, string UserSecLevel)
+    {
+        bool recordSaved;
+
+        try
+        {
+            // creates new connection to database
+            OleDbConnection conn = new OleDbConnection("PROVIDER=Microsoft.ACE.OLEDB.12.0;" +
+            "Data Source=" + Database);
+            conn.Open();
+            OleDbCommand command = conn.CreateCommand();
+            string strSQL;
+
+            // creates a SQL string to be inserted into the Users table
+            strSQL = "Insert into Users " +
+            "(UserLogon, UserPassword, UserSecLevel) values ('" +
+            UserLogon + "', '" + UserPassword + "', '" + UserSecLevel + "')";
+
+            // issues a SQl string command type
+            command.CommandType = CommandType.Text;
+            command.CommandText = strSQL;
+
+            // executes an SQL statement
+            command.ExecuteNonQuery();
+
+            // closes the connection to the data source
+            conn.Close();
+
+            recordSaved = true;
+        }
+        catch (Exception ex)
+        {
+            // catches the new data input if incorrect and rollsback to the previous dataset
+            // myTransaction.Rollback();
+            recordSaved = false;
+        }
+        return recordSaved;
+
+    }
+
+    public static bool SaveUser(string v, string text1, string text2, object p)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ week 4 login + MyAccount Updated/Williams Specialty Company/OrderInformation.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="OrderInformation.aspx.cs" Inherits="OrderInformation" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:ImageButton ID="ImageButton1" runat="server" ImageUrl="~/images/Logo.png" PostBackUrl="~/Main.aspx" />
+        <br />
+        <asp:Label ID="lblOrderInformation" runat="server" Text="This is the OrderInformation page for operations managers"></asp:Label>
+    
+        <br />
+        <br />
+        <asp:GridView ID="grdOrderInformation" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+            <AlternatingRowStyle BackColor="White" />
+            <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Center" />
+            <RowStyle BackColor="#FFFBD6" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="Navy" />
+            <SortedAscendingCellStyle BackColor="#FDF5AC" />
+            <SortedAscendingHeaderStyle BackColor="#4D0000" />
+            <SortedDescendingCellStyle BackColor="#FCF6C0" />
+            <SortedDescendingHeaderStyle BackColor="#820000" />
+        </asp:GridView>
+    
+    </div>
+    </form>
+</body>
+</html>
