9e79f8444b8ebb6472d963025b853c5848d63f00
Joeyrmuzzo <57329326+Joeyrmuzzo@users.noreply.github.com>
2019-12-13 21:29:19 -0500

    Add files via upload

---
+++ Williams Specialty Company Project Complete maybe/Main.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Main : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session[ "SecurityLevel"] == null)// is user is not signed in then make the login button visible and logout invisible else make logout visible and login invisible
+        {
+            btnLogin.Visible = true;
+            btnLogout.Visible = false;
+        }
+        else
+        {
+            btnLogin.Visible = false;
+            btnLogout.Visible = true;
+        }
+        if (Session["SecurityLevel"] == "C")//added 12/10/19 hides the order information button if securirty level is customer - Joey Muzzo
+        {
+            btnOrderInfo.Visible = false;
+            btnCreateUser.Visible = false;
+
+        }
+        if (Session["SecurityLevel"] == "O")//added 12/10/19 hides the order information button if securirty level is customer - Joey Muzzo
+        {
+            btnPlaceOrder.Visible = false;
+            btnMyAccount.Visible = false;
+
+        }
+    }
+
+
+    protected void btnLogin_Click(object sender, ImageClickEventArgs e)
+    {
+
+    }
+
+    protected void btnLogout_Click(object sender, ImageClickEventArgs e)
+    {
+        Session["SecurityLevel"] = null;
+        Session["CustID"] = null;
+        Session["AddressID"] = null;
+        Session["UserNameID"] = null;
+    }
+}
